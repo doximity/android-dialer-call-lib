3ef010065790279285c49042d69d885b73ec2c56
Kurt Mueller <kmueller@doximity.com>
2023-12-12 16:48:44 -0800

    [ANDDOX-6531] updated to AndroidX.

--- CallWithDoxDialerLib/src/main/java/com/doximity/callwithdoxdialerlib/DoxDialerCaller.java
+++ CallWithDoxDialerLib/src/main/java/com/doximity/callwithdoxdialerlib/DoxDialerCaller.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 /**
  * Created by yinanoliver on 5/11/17.
